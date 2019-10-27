8ebde20c196297a6536e840373d89db503de3fba
Vyshydkevich Katsiaryna <kvyshydkevich@exadel.net>
2019-10-27 22:19:17 +0300

    task2

--- HW1T2_1.js
+++ HW1T2_1.js
@@ -3,23 +3,28 @@ function isPalindrome(str){
     let repl = /[^a-zA-Z0-9]/gi    
     let lowStr = str.replace(repl, "").toLowerCase(); // Bring all letters to lower case and remove special characters
     
-   
     let reversedPalindrome = lowStr.split('').reverse().join(''); //Reversing a string
       
-    console.log(str + " is " +
-    ((lowStr === reversedPalindrome) ? "" : "not ") +  "a palindrome");
-    // Checking if reversed string is equal to modified string and showing results
-    }
+    return(lowStr === reversedPalindrome);  // Checking if reversed string is equal to modified string and return results
+}
 
- isPalindrome('Race fast, safe car');
+ 
 
- isPalindrome('Anita lava la tina');
+function printCons(s){
 
- isPalindrome('lalalaltratata');
+    console.log(s + " - is " + ((isPalindrome(s)) ? "" : "not ") +  "a palindrome"); //prints result into console
 
- isPalindrome('A roza upala na lapu Azora');
+}
 
+let s1='Race fast, safe car';
 
+let s2='Anita lava la tina';
 
+let s3='lalalaltratata';
 
+let s4='A roza upala na lapu Azora';
 
+printCons(s1);
+printCons(s2);
+printCons(s3);
+printCons(s4);
