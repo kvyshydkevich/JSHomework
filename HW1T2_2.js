5d901528727ade53085c243c43f135d8e68091eb
Vyshydkevich Katsiaryna <kvyshydkevich@exadel.net>
2019-10-27 21:41:03 +0300

    test

--- HW1T2_2.js
+++ HW1T2_2.js
@@ -1,22 +1,19 @@
-
 function isPalindrome(str){
-   let repl = /[^a-zA-Z0-9]/gi    
-   let lowStr = str.toLowerCase().replace(repl, ""); // Bring all letters to lower case and remove special characters
-   let l = lowStr.length;
-
-   for (let i = 0; i < l/2; i++) {//Checking the [i] and [l - 1 - i] characters
-      if (lowStr[i] !== lowStr[l - 1 - i]) { //When the characters don't match we exit loop and writing in console results
-        console.log(str + " - not a palindrome "); 
-        return; 
+    let repl = /[^a-zA-Z0-9]/gi    
+    let lowStr = str.toLowerCase().replace(repl, ""); // Bring all letters to lower case and remove special characters
+    let l = lowStr.length;
+ 
+    for (let i = 0; i < l/2; i++) {//Checking the [i] and [l - 1 - i] characters
+       if (lowStr[i] !== lowStr[l - 1 - i]) { //When the characters don't match we exit loop and returning result
+            return str + ' - not a palindrome'; 
         }
-    }
-    console.log(str + " - a palindrome "); //if characters matching - writing in console results
- }
-
-isPalindrome('Race fast, safe car');
-
-isPalindrome('Anita lava la tina');
-     
-isPalindrome('lalalaltratata');
-
-isPalindrome('A roza upala na lapu Azora');
+     }
+ 
+    return str + ' - a palindrome'; //if characters matching - returning result
+  }
+ 
+  console.log(isPalindrome('Race fast, safe car'));
+   
+  console.log(isPalindrome('lalalaltratata'));
+ 
+  console.log(isPalindrome('A roza upala na lapu Azora'));
