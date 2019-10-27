c605b5cdfd9215da4c58f02368899154ddd40e39
Vyshydkevich Katsiaryna <kvyshydkevich@exadel.net>
2019-10-27 19:52:36 +0300

    test

--- tes.js
+++ tes.js
@@ -1,1 +1,24 @@
-console.log('test');
+function isPalindrome(str){
+
+    var repl = /[^a-zA-Z0-9]/gi    
+    var lowStr = str.replace(repl, "").toLowerCase(); // Bring all letters to lower case and remove special characters
+    
+   
+    var reversedPalindrome = lowStr.split('').reverse().join(''); //Reversing a string
+      
+    console.log(str + " is " +
+    ((lowStr === reversedPalindrome) ? "" : "not ") +
+    "a palindrome");
+    // Check if reversed string is equal to modified string and showing results
+    }
+
+      isPalindrome('Race fast, safe car')
+
+      isPalindrome('Anita lava la tina')
+
+      isPalindrome('A roza upala na lapu Azora')
+
+      isPalindrome('lalalaltratata')
+
+
+
